724cc070c96ed1f83cb4b5fa6bdf6d9b921227ac
Whiting <whitinghtennis@gmail.com>
2021-08-10 10:45:24 -0700

    commit tree 81fd9dbbdd80e9e0447eeb206e5168ccd284ced5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == '.'
   end
 
   def question?
-
+    self[-1] == '?'
   end
 
   def exclamation?
-
+    self[-1] == '!'
   end
 
   def count_sentences
-
+    # puts 'count_sentences called'
+    self.split(/[!?.]/).reject(&:empty?).length
   end
+
+  # def count_sentences
+  #   puts 'count_sentences called'
+  #   delimiters = ['.', '!', '?']
+  #   self.split(Regexp.union(delimiters)).length
+  # end
 end
+
+
+
+'This, well, is a sentence.. This is too!! And so is this, I think? Woo...'.count_sentences
+# require './lib/count_sentences.rb'
